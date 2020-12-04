5c0f45354f069fe5ad1aa5a0bf6b6dd9c339a686
jul6art <geoffreyk42@gmail.com>
2020-12-04 03:47:52 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HTTP } from '@ionic-native/http/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,6 +18,7 @@ import { AppComponent } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
+    HTTP,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HTTP } from '@ionic-native/http/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  ip1: number;
+  ip2: number;
+  ip3: number;
+  ip4: number;
 
-  constructor() {}
+  constructor(private http: HTTP) {
+    this.ip1 = 192;
+    this.ip2 = 168;
+    this.ip3 = 0;
+    this.ip4 = 8;
 
+    this.call('D1', false);
+    this.call('D2', false);
+    this.call('D3', false);
+  }
+
+  public call(light: string, enabled: boolean): void
+  {
+    const url = 'http://' + this.ip1 + '.' + this.ip2 + '.' + this.ip3 + '.' + this.ip4 + '/' + light + '/' + (enabled ? 1 : 0);
+
+    console.log(enabled);
+    console.log(url);
+
+    this.http.get(url, {}, {})
+        .then(data => {
+
+          console.log(data.status);
+          console.log(data.data); // data received by server
+          console.log(data.headers);
+
+        })
+        .catch(error => {
+
+          console.log(error.status);
+          console.log(error.error); // error message as string
+          console.log(error.headers);
+
+        });
+  }
 }
